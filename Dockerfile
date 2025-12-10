46c576a2b560864fd4b951558c7a1ab5476826ff
Raisssa Geovana <raissalimmaa@gmail.com>
2025-12-10 12:27:56 -0300

    Fix PORT variable in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:$PORT"]
+CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:${PORT:-5000}"]
--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def home():
     return {"mensagem": "API EntomoTrack conectada ao PostgreSQL!"}
 
 if __name__ == "__main__":
-    # Para produção no Render
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
